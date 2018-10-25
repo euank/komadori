4a31c1455b47b55b6c328e0d4d8346d9a315a8b2
Euan Kemp <euank@euank.com>
2018-10-25 12:41:10 -0700

    frontend: add milligram css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import 'milligram/dist/milligram.min.css';
 import './index.css';
 import './site.css';
 import App from './containers/App';
